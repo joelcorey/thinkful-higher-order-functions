8fdb374f947e66e39286172f50bc1d57c82cdf31
Joel Corey <joelcorey@fastmail.com>
2019-02-19 15:25:34 -0800

    Fix function

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 'use strict';
 
-function repeat(fn, loopNumber){
-  for (let i = 0; i < loopNumber; i++) {
-    fn(); 
+// function repeat(fn, loopNumber){
+//   for (let i = 0; i < loopNumber; i++) {
+//     fn(); 
+//   }
+// }
+// function hello(){
+//   console.log('Hello world');
+// }
+
+// function goodbye(){
+//   console.log('Goodbye world');
+// }
+
+// repeat(goodbye, 5);
+
+function filter(inArray, fn) {
+  let newArray = [];
+  for (let i = 0; i < inArray.length; i++) {
+    if(fn(inArray[i])){
+      newArray.push(inArray[i]);
+    }
   }
+  return newArray;
 }
 
-function hello(){
-  console.log('Hello world');
-}
+// DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
+// First we setup an array of strings we plan to filter:
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 
-function goodbye(){
-  console.log('Goodbye world');
-}
+// We use your `filter` function here, capturing a new array into `filteredNames`
+// comprised of names that only begin with 'R'
+const filteredNames = filter(myNames, function(name) {
+  // This is known as a "predicate function" - it's a function that 
+  // only returns a boolean
+  return name[0] === 'R';
+});
 
-repeat(goodbye, 5);
+console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
