85911e2b9230d85bf7519abc6373397fcdaed003
Joel Corey <joelcorey@fastmail.com>
2019-02-20 06:01:51 -0800

    Code clean

--- index.js
+++ index.js
@@ -1,80 +1,92 @@
 'use strict';
 
-// function repeat(fn, loopNumber){
-//   for (let i = 0; i < loopNumber; i++) {
-//     fn(); 
-//   }
-// }
-// function hello(){
-//   console.log('Hello world');
-// }
-// function goodbye(){
-//   console.log('Goodbye world');
-// }
-// repeat(goodbye, 5);
+// Start function as argument
+function repeat(fn, loopNumber){
+  for (let i = 0; i < loopNumber; i++) {
+    fn(); 
+  }
+}
+function hello(){
+  console.log('Hello world');
+}
+function goodbye(){
+  console.log('Goodbye world');
+}
+// Uncomment this line to test
+//repeat(goodbye, 5);
+// End function as argument
 
-// function filter(inArray, fn) {
-//   let newArray = [];
-//   for (let i = 0; i < inArray.length; i++) {
-//     if(fn(inArray[i])){
-//       newArray.push(inArray[i]);
-//     }
-//   }
-//   return newArray;
-// }
-// // DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
-// // First we setup an array of strings we plan to filter:
-// const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
-// // We use your `filter` function here, capturing a new array into `filteredNames`
-// // comprised of names that only begin with 'R'
-// const filteredNames = filter(myNames, function(name) {
-//   // This is known as a "predicate function" - it's a function that 
-//   // only returns a boolean
-//   return name[0] === 'R';
-// });
+// Start rocksWarning
+function filter(inArray, fn) {
+  let newArray = [];
+  for (let i = 0; i < inArray.length; i++) {
+    if(fn(inArray[i])){
+      newArray.push(inArray[i]);
+    }
+  }
+  return newArray;
+}
+// DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
+// First we setup an array of strings we plan to filter:
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+// We use your `filter` function here, capturing a new array into `filteredNames`
+// comprised of names that only begin with 'R'
+const filteredNames = filter(myNames, function(name) {
+  // This is known as a "predicate function" - it's a function that 
+  // only returns a boolean
+  return name[0] === 'R';
+});
 
-// console.log(filteredNames) // => ['Rich', 'Ray']
-// // <---- DO NOT EDIT BETWEEN THESE LINES
+//console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
 
-// function hazardWarningCreator(typeOfWarning){
-//   let counter = 0;
-//   return function(location){
-//     counter++;
-//     let warningMessage = `DANGER! There is a ${typeOfWarning} hazard at ${location}!`;
-//     let warningCounter = `The ${typeOfWarning} hazard alert has triggered ${counter} time(s) today!`;
-//     console.log(warningMessage);
-//     console.log(warningCounter);
-//     //return `${warningMessage} ${warningCounter}`;
-//   };
-// }
+function hazardWarningCreator(typeOfWarning){
+  let counter = 0;
+  return function(location){
+    counter++;
+    let warningMessage = `DANGER! There is a ${typeOfWarning} hazard at ${location}!`;
+    let warningCounter = `The ${typeOfWarning} hazard alert has triggered ${counter} time(s) today!`;
+    console.log(warningMessage);
+    console.log(warningCounter);
+    //return `${warningMessage} ${warningCounter}`;
+  };
+}
 
-// const rocksWarning = hazardWarningCreator('Rocks on the Road');
+// Uncomment these lines for this section to work:
+//const rocksWarning = hazardWarningCreator('Rocks on the Road');
+//rocksWarning('Main St and Pacific Ave');
+//rocksWarning('Pacific Ave and East Main St');
+//rocksWarning('1st and Broadway');
 
-// rocksWarning('Main St and Pacific Ave');
-// rocksWarning('Pacific Ave and East Main St');
-// rocksWarning('1st and Broadway');
+// End rocksWarning
 
-// function removeNegatives(el){
-//   return (el[0] >= 0 && el[1] >= 0);
-// }
+// Start array filter map
 
-// function combine(el){
-//   let combined = []
-//   combined.push(el[0] + el[1]);
-//   return combined;
-// }
+function removeNegatives(el){
+  return (el[0] >= 0 && el[1] >= 0);
+}
 
-// let turtleMovements = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
-//removeNegatives(turtleMovements);
-// let turtleFilter = turtleMovements.filter(function (el) {
-//   return (el[0] >= 0 && el[1] >= 0);
-// });
+function combine(el){
+  let combined = []
+  combined.push(el[0] + el[1]);
+  return combined;
+}
 
-// let turtleFiltered = turtleMovements.filter(removeNegatives);
-// let turtleMapped = turtleFiltered.map(combine);
+let turtleMovements = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
+removeNegatives(turtleMovements);
+let turtleFilter = turtleMovements.filter(function (el) {
+  return (el[0] >= 0 && el[1] >= 0);
+});
+
+let turtleFiltered = turtleMovements.filter(removeNegatives);
+let turtleMapped = turtleFiltered.map(combine);
+// Uncommment these lines to test
 // console.log(turtleFiltered);
 // console.log(turtleMapped);
 
+// End array filter map
+
+// Start array reduce
 let input = 'noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest'.split(' ');
 //console.log(input);
 
@@ -82,12 +94,6 @@ let derp = input.reduce((accumulater, currentValue) => {
   return currentValue.length === 3 ? accumulater + ' ' : accumulater + currentValue.substring(currentValue.length - 1, currentValue.length).toLocaleUpperCase();
 }, '');
 
-// let derp = input.reduce((accumulater, currentValue) => {
-//   if(currentValue.length === 3){
-//     return accumulater + ' ';
-//   } else {
-//     return accumulater + currentValue.substring(currentValue.length - 1, currentValue.length).toLocaleUpperCase();
-//   }
-// }, '');
-
 console.log(derp);
+
+// End array reduce
